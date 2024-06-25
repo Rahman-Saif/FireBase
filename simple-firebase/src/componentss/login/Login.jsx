6802f974852ea526d5d78f44cd780655402f1fb6
Rahman <rahmansaif.413@gmail.com>
2024-06-26 00:39:23 +0600

    github authentication in simple-firebase

--- simple-firebase/src/componentss/login/Login.jsx
+++ simple-firebase/src/componentss/login/Login.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../firebase/firebase_init';
 
 const Login = () => {
     const [user,setUser]=useState(null);
     const auth = getAuth(app);
     console.log(app);
-    const provider=new GoogleAuthProvider;
+    const googleProvider=new GoogleAuthProvider;
+    const githubProvider=new GithubAuthProvider();
 
     const handleGoogleSignIn=()=>{
-        signInWithPopup(auth,provider)
+        signInWithPopup(auth, googleProvider)
+          .then((result) => {
+            const loggedInuser = result.user;
+            console.log(user);
+            setUser(loggedInuser);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }
+
+    const handleGithubSignIn=()=>{
+        signInWithPopup(auth,githubProvider)
         .then(result=>{
             const loggedInuser=result.user;
-            console.log(user);
+            console.log(loggedInuser);
             setUser(loggedInuser);
         })
         .catch(error=>{
@@ -31,17 +44,22 @@ const Login = () => {
         })
     }
     return (
-        <div>
-            {
-                user?
-            <button onClick={handleGoogleSignIn}>Google Login</button>:
-            <button onClick={handleSignOut}>Sign Out</button>
-            }
-            { user && <div>
-                <h3>User:{user.displayName}</h3>
-                <p>Email:{user.email}</p>
-            </div>}
-        </div>
+      <div>
+        {user ? (
+          <button onClick={handleSignOut}>Sign Out</button>
+        ) : (
+          <div>
+            <button onClick={handleGoogleSignIn}>Google Login</button>
+            <button onClick={handleGithubSignIn}>Github Login</button>
+          </div>
+        )}
+        {user && (
+          <div>
+            <h3>User:{user.displayName}</h3>
+            <p>Email:{user.email}</p>
+          </div>
+        )}
+      </div>
     );
 };
 
